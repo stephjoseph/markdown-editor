31305c4cf67753e7e33c8c0b1de2f67bb46c8f71
Stephen Muñez <sjtmunez@gmail.com>
2023-09-18 22:49:15 +0800

    feat: markdown editor example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="editor">
+    <textarea class="input" :value="input" @input="update"></textarea>
+    <div class="output" v-html="output"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { marked } from 'marked';
+import { debounce } from 'lodash-es';
+
+export default {
+  data: () => ({
+    input: '# hello',
+  }),
+  computed: {
+    output() {
+      return marked(this.input);
+    },
+  },
+  methods: {
+    update: debounce(function (e) {
+      this.input = e.target.value;
+    }, 100),
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  color: black;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.editor {
+  height: 100vh;
+  display: flex;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.input,
+.output {
+  overflow: auto;
+  width: 50%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+.input {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
 }
 </style>
